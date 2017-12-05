60898c2e3d2fbe5692ed1c52a530f758bac20fda
wangsdchn <wangsdbit@163.com>
2017-12-05 17:49:05 +0800

    scale

--- ImageViewer.py
+++ ImageViewer.py
@@ -4,40 +4,51 @@ Created on Fri Dec  1 09:19:19 2017
 
 @author: WSD
 """
-
 import sys
- 
+import cv2
 #这里我们提供必要的引用。基本控件位于pyqt5.qtwidgets模块中。
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox, QPushButton,QGridLayout,QLabel,QSplitter
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,QLabel,QHBoxLayout,QVBoxLayout,QLayout
+from PyQt5.QtGui import QPixmap,QImage
 #%%
  
 class ImageViewer(QWidget):
     def __init__(self):
         super().__init__()
         self.scale = 1.0
+        self.image = None
         self.initUI()  
     def initUI(self):
         self.readImgBtn = QPushButton('ReadImg')
-        #self.readImgBtn.setFixedSize(self.readImgBtn.size())
+        self.readImgBtn.setMaximumSize(500,30)
         self.showImgLabel = QLabel('image')
         self.showImgLabel.setAlignment(Qt.AlignCenter)
         self.showImgLabel.setMouseTracking(True)
-       
-        self.image = QPixmap()
-               
-        self.splitterRight = QSplitter(Qt.Vertical)
-        self.splitterRight.addWidget(self.showImgLabel)
-        self.splitterMain = QSplitter(Qt.Horizontal,self)
-        self.splitterMain.addWidget(self.readImgBtn)
-        self.splitterMain.addWidget(self.splitterRight)
+        self.showImgLabel.setFixedSize(640,480)
         
-        self.splitterMain.setStretchFactor(0,0)
-        self.splitterMain.setStretchFactor(1,1)
-        self.splitterMain.resize(800,600)
+        self.leftLayout = QVBoxLayout()
+        self.leftLayout.addWidget(self.readImgBtn)
+        self.leftLayout.setSpacing(6)
+        self.rightLayout = QVBoxLayout()
+        self.rightLayout.addWidget(self.showImgLabel)
+        #self.rightLayout.setSpacing(6)
+        self.rightLayout.setSizeConstraint(QLayout.SetFixedSize)
+        self.mainLayout = QHBoxLayout(self)
+        self.mainLayout.addLayout(self.leftLayout)
+        self.mainLayout.addLayout(self.rightLayout)
+        self.mainLayout.setStretchFactor(self.leftLayout,1)
+        self.mainLayout.setStretchFactor(self.rightLayout,5)
+        self.mainLayout.setSpacing(6)
+        
+        self.sizeWin = self.size()
         self.readImgBtn.clicked.connect(self.readImage)
 
+    def resizeEvent(self,event): 
+        sizeold=self.sizeWin
+        #self.sizeWin = self.size()
+        #sizedif = self.sizeWin-sizeold
+        #self.showImgLabel.setFixedSize(640,480)
+        #self.showImgLabel.resize(640+sizedif[0],480+sizedif[1])
     def wheelEvent(self,event):
         delta = event.angleDelta()
         numDegress = delta.y()/8
@@ -47,10 +58,19 @@ class ImageViewer(QWidget):
             self.scale /= 1.2
         self.showImage()
     def showImage(self):
-        self.showImgLabel.setPixmap(self.image.scaled(800*self.scale,600*self.scale,Qt.KeepAspectRatio))
+        h,w,c = self.image.shape
+        scaleMat = cv2.resize(self.image,(int(self.scale*w),int(self.scale*h)),interpolation = cv2.INTER_AREA)
+        h,w,c = scaleMat.shape
+        bytesPerLine = c * w
+        self.qimage=QImage(scaleMat.data,w,h,bytesPerLine,QImage.Format_RGB888)
+        self.showImgLabel.setPixmap(QPixmap.fromImage(self.qimage))
     def readImage(self):
-        self.image=QPixmap('E:/wsd/index.jpg')
-        self.showImgLabel.setPixmap(self.image)
+        self.image=cv2.imread('E:/wsd/index.jpg')
+        cv2.cvtColor(self.image,cv2.COLOR_BGR2RGB,self.image)
+        height, width, bytesPerComponent = self.image.shape
+        bytesPerLine = bytesPerComponent * width
+        self.qimage = QImage(self.image.data,width,height,bytesPerLine,QImage.Format_RGB888)
+        self.showImgLabel.setPixmap(QPixmap.fromImage(self.qimage))
     """
     def closeEvent(self,event):
         reply = QMessageBox.question(self,'Message','Are you sure to exit?',QMessageBox.Yes|QMessageBox.No,QMessageBox.No)

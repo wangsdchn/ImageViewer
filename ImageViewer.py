403c43df844ef92a11b1af10dd3d36ba13cce1bf
wangsdchn <wangsdbit@163.com>
2017-12-07 14:18:43 +0800

    mouse

--- ImageViewer.py
+++ ImageViewer.py
@@ -7,7 +7,7 @@ Created on Fri Dec  1 09:19:19 2017
 import sys
 import cv2
 #这里我们提供必要的引用。基本控件位于pyqt5.qtwidgets模块中。
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt,QPoint,QRect,QSize
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,QLabel,QHBoxLayout,QVBoxLayout,QLayout,QScrollArea
 from PyQt5.QtGui import QPixmap,QImage
 #%%
@@ -17,7 +17,9 @@ class ImageViewer(QWidget):
         super().__init__()
         self.scale = 1.0
         self.image = None
-        self.initUI()  
+        self.bMousePress = False
+        self.pressPos = None
+        self.initUI()
     def initUI(self):
         self.readImgBtn = QPushButton('ReadImg')
         self.readImgBtn.setMaximumSize(500,30)
@@ -28,6 +30,8 @@ class ImageViewer(QWidget):
         self.scrollArea = QScrollArea()
         self.scrollArea.setWidget(self.showImgLabel)
         self.scrollArea.setAlignment(Qt.AlignCenter)
+        self.scrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         
         self.leftLayout = QVBoxLayout()
         self.leftLayout.addWidget(self.readImgBtn)
@@ -45,13 +49,39 @@ class ImageViewer(QWidget):
         self.readImgBtn.clicked.connect(self.readImage)
 
     def wheelEvent(self,event):
-        delta = event.angleDelta()
-        numDegress = delta.y()/8
-        if numDegress > 0:
-            self.scale *= 1.2
-        else:
-            self.scale /= 1.2
-        self.showImage()
+        x = self.showImgLabel.geometry().x() + self.rightLayout.geometry().x()
+        y = self.showImgLabel.geometry().y() + self.rightLayout.geometry().y()
+        w,h = self.showImgLabel.geometry().width(),self.showImgLabel.geometry().height()
+        qrect = QRect(QPoint(x,y),QSize(w,h))
+        if qrect.contains(event.pos()):
+            delta = event.angleDelta()
+            numDegress = delta.y()/8
+            if numDegress > 0:
+                self.scale *= 1.2
+            else:
+                self.scale /= 1.2
+            self.showImage()
+    def mouseMoveEvent(self,event):
+        if not self.bMousePress:
+            return
+        curPt = event.pos()
+        disX = self.pressPos.x() - curPt.x()
+        disY = self.pressPos.y() - curPt.y()
+        self.scrollArea.verticalScrollBar().setValue(self.scrollArea.verticalScrollBar().value() + disY)
+        self.scrollArea.horizontalScrollBar().setValue(self.scrollArea.horizontalScrollBar().value() + disX)
+        self.pressPos = curPt
+    def mousePressEvent(self,event):
+        x = self.showImgLabel.geometry().x() + self.rightLayout.geometry().x()
+        y = self.showImgLabel.geometry().y() + self.rightLayout.geometry().y()
+        w,h = self.showImgLabel.geometry().width(),self.showImgLabel.geometry().height()
+        qrect = QRect(QPoint(x,y),QSize(w,h))
+        if qrect.contains(event.pos()) and Qt.LeftButton == event.buttons():
+            #self.setCursor(Qt.ClosedHandCursor)
+            self.bMousePress = True
+            self.pressPos = event.pos()
+    def mouseReleaseEvent(self,event):
+        self.bMousePress = False
+        self.pressPos = QPoint(0,0)
     def showImage(self):
         h,w,c = self.image.shape
         scaleMat = cv2.resize(self.image,(int(self.scale*w),int(self.scale*h)),interpolation = cv2.INTER_AREA)
